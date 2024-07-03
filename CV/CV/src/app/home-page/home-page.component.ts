05dc95554b6fa6b8429a7b6e96981e41891851d2
Michael Combrinck <119591159+MichaelCombrinck@users.noreply.github.com>
2024-07-03 15:29:04 +0200

    fix(nav): active is now working

--- CV/CV/src/app/home-page/home-page.component.ts
+++ CV/CV/src/app/home-page/home-page.component.ts
@@ -1,19 +1,50 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 import { ContactInfoComponent } from '../contact-info/contact-info.component';
+import { Console } from 'console';
+import { CommonModule } from '@angular/common';
 
-
+export interface NavItems {
+  Name: string;
+  IsActive: boolean;
+}
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatDividerModule, ContactInfoComponent],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    ContactInfoComponent,
+    CommonModule      
+  ],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
+
+  public topNavLinkIsActive: NavItems[] = [
+    {
+      Name: 'Home',
+      IsActive: true,
+    },
+    {
+      Name: 'About Me',
+      IsActive: false,
+    },
+    {
+      Name: 'Skills',
+      IsActive: false,
+    },
+    {
+      Name: 'Qualifications',
+      IsActive: false,
+    },
+
+  ];
 
   public skills = [
     { name: 'Angular', level: 'Advanced' },
@@ -24,6 +55,23 @@ export class HomePageComponent {
     { name: 'JavaScript', level: 'Advanced' },
     { name: 'TypeScript', level: 'Advanced' },
     { name: 'MVC', level: 'Intermediate' },
-    { name: 'C# WPF', level: 'Beginner' }
+    { name: 'C# WPF', level: 'Beginner' },
   ];
+
+  onNavClick(index: number): void {
+    // Toggle the clicked item
+    this.topNavLinkIsActive[index].IsActive = !this.topNavLinkIsActive[index].IsActive;
+
+    // Ensure only one item can be active at a time
+    if (this.topNavLinkIsActive[index].IsActive) {
+      this.topNavLinkIsActive.forEach((item, i) => {
+        if (i !== index) {
+          item.IsActive = false;
+        }
+      });
+    }
+    
+    console.log('Index ', index);
+    console.log('The value of the top nav items: ', this.topNavLinkIsActive);
+  }
 }
