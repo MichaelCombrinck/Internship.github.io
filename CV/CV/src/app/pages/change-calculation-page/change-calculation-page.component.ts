24e9fdde945613bda5e2d1d982def2efd02ae9d6
Michael Combrinck <119591159+MichaelCombrinck@users.noreply.github.com>
2024-07-19 08:59:06 +0200

    feat(checkout): added calculation section

--- CV/CV/src/app/pages/change-calculation-page/change-calculation-page.component.ts
+++ CV/CV/src/app/pages/change-calculation-page/change-calculation-page.component.ts
@@ -4,9 +4,11 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router } from '@angular/router';
-import { Product } from '../../core/models/product';
 import { ProductService } from '../../services/product.service';
 import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-change-calculation-page',
@@ -17,12 +19,19 @@ import { MatTableModule } from '@angular/material/table';
     MatDividerModule,
     MatButtonModule,
     MatTableModule,
+    MatFormFieldModule, 
+    FormsModule,
+    MatInputModule
   ],
   templateUrl: './change-calculation-page.component.html',
   styleUrl: './change-calculation-page.component.scss',
 })
 export class ChangeCalculationPageComponent implements OnInit {
   public totalOfCard: number = 0;
+
+  public amountPaid: number = 0;
+
+  public changeMessage: string | null = null;
 
   public displayedColumns: string[] = [
     'imageUrl',
@@ -57,5 +66,28 @@ export class ChangeCalculationPageComponent implements OnInit {
       (sum, current) => sum + current.price * current.quantity,
       0
     );
+  }
+
+  calculateChange() {
+    if (this.amountPaid < this.totalOfCard) {
+      this.changeMessage = 'Insufficient amount paid.';
+      return;
+    }
+
+    let change = this.amountPaid - this.totalOfCard;
+    const intervals = [200, 50, 10, 5, 1, 0.5, 0.1];
+    let changeBreakdown: { [key: string]: number } = {};
+
+    intervals.forEach(interval => {
+      if (change >= interval) {
+        const count = Math.floor(change / interval);
+        changeBreakdown[interval] = count;
+        change = Number((change % interval).toFixed(2));
+      }
+    });
+
+    this.changeMessage = `Change: R${(this.amountPaid - this.totalOfCard).toFixed(2)}. Denominations: ${Object.entries(changeBreakdown)
+      .map(([key, value]) => `${value} x R${key}`)
+      .join(', ')}`;
   }
 }
